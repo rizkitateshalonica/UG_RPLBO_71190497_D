a3d429af9bf422ceb2da169f08f408453e8f1be8
rizkitateshalonica <rizkita.teshalonica@ti.ukdw.ac.id>
2022-02-16 17:35:09 +0700

    Create PejalanKaki.java
    71190497 SOAL 1 - Class PejalanKaki

---
+++ UG2_71190497/71190497_1/PejalanKaki.java
@@ -0,0 +1,15 @@
+package ug2;
+
+public class PejalanKaki {
+	String nama;
+	
+	public void menyeberang() {
+		System.out.println("Pejalan kaki melewati zebra cross . . .");
+	}
+	public void bersiap() {
+		System.out.println("Pejalan kaki bersiap . . .");
+	}
+	public void menunggu() {
+		System.out.println("Pejalan kaki menunggu lampu hijau . . .");
+	}
+}
