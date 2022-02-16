389305aa5c5dfbe60cc505ebdfb6e177cf0a745a
rizkitateshalonica <rizkita.teshalonica@ti.ukdw.ac.id>
2022-02-16 17:34:56 +0700

    Create Mobil.java
    71190497 SOAL 1 - Class Mobil

---
+++ UG2_71190497/71190497_1/Mobil.java
@@ -0,0 +1,16 @@
+package ug2;
+
+public class Mobil {
+	String jenis;
+	String mesin;
+	
+	public void jalan() {
+		System.out.println("Mobil jalan . . .");
+	}
+	public void kurangiKecepatan() {
+		System.out.println("Korangi kecepatan mobil . . .");
+	}
+	public void berhenti() {
+		System.out.println("Mobil berhenti . . .");
+	}
+}
